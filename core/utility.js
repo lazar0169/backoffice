aad4adf19d7c91ac406ed49e6f37702d35d119a6
Lazar Manasijević <lazar0169@gmail.com>
2018-09-12 08:49:07 +0200

    SIdebar and navbar added

--- core/utility.js
+++ core/utility.js
@@ -18,3 +18,26 @@ let isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.
 let isFirefox = typeof InstallTrigger !== 'undefined';
 let isSafari = navigator.userAgent.match(/Version\/[\d\.]+.*Safari/);
 let isEdge = /Edge\/\d./i.test(navigator.userAgent);
+
+let urlData = function () {
+    let data = getQueryParams(window.location.search);
+    return {
+        getItem: function (key) {
+            if (data[key] === undefined)
+                return '';
+            else
+                return data[key];
+        }
+    };
+}();
+
+function getQueryParams(qs) {
+    qs = qs.split('+').join(' ');
+    var params = {},
+        tokens,
+        re = /[?&]?([^=]+)=([^&]*)/g;
+    while (tokens = re.exec(qs)) {
+        params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
+    }
+    return params;
+}
