e9995d5ffd484e615ce27846f6b642d1da4757b0
Lazar Manasijević <lazar0169@gmail.com>
2018-09-21 12:09:10 +0200

    Login design completed

--- scripts/login.js
+++ scripts/login.js
@@ -1,5 +1,11 @@
 const login = function () {
+    let loginBtn = $$('#login-btn');
     on('resize', function () {
         document.body.classList[isMobile ? 'add' : 'remove']('mobile');
+    });
+    window.addEventListener('keyup', function (event) {
+        if (event.keyCode === 13) {
+            loginBtn.click();
+        }
     });
 }();
