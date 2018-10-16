0d07bb1702112f5095765f2f08f99bf40221aaa7
Lazar Manasijević <lazar0169@gmail.com>
2018-10-16 13:42:52 +0200

    Operators added and currency module prepared

---
+++ scripts/currency.js
@@ -0,0 +1,11 @@
+const currency = (function () {
+    let list = $$('.currency');
+    let currencies = ['EUR', 'USD', 'RSD',];
+    let selected = 0;
+
+    for (let cur of list) {
+        cur.parentNode.addEventListener('click', function () {
+            cur.innerHTML = currencies[++selected && (selected %= currencies.length)];
+        });
+    }
+})();
