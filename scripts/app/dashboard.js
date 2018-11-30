6d439b24206d6574ca911dabb44414d219cd80c0
Lazar Manasijević <lazar0169@gmail.com>
2018-11-30 09:47:49 +0100

    Dashboard: chart pie fixed labels

--- scripts/app/dashboard.js
+++ scripts/app/dashboard.js
@@ -23,7 +23,36 @@ let dashboard = function () {
             legend: {
                 position: 'right',
                 labels: {
-                    fontColor: 'white'
+                    fontColor: 'white',
+                    generateLabels: function (chart) {
+                        let data = chart.data;
+                        if (data.labels.length && data.datasets.length) {
+                            return data.labels.map(function (label, i) {
+                                let meta = chart.getDatasetMeta(0);
+                                let ds = data.datasets[0];
+                                let arc = meta.data[i];
+                                let custom = arc && arc.custom || {};
+                                let getValueAtIndexOrDefault = Chart.helpers.getValueAtIndexOrDefault;
+                                let arcOpts = chart.options.elements.arc;
+                                let fill = custom.backgroundColor ? custom.backgroundColor : getValueAtIndexOrDefault(ds.backgroundColor, i, arcOpts.backgroundColor);
+                                let stroke = custom.borderColor ? custom.borderColor : getValueAtIndexOrDefault(ds.borderColor, i, arcOpts.borderColor);
+                                let bw = custom.borderWidth ? custom.borderWidth : getValueAtIndexOrDefault(ds.borderWidth, i, arcOpts.borderWidth);
+
+                                let value = chart.config.data.datasets[arc._datasetIndex].data[arc._index];
+
+                                return {
+                                    text: label + " : " + value,
+                                    fillStyle: fill,
+                                    strokeStyle: stroke,
+                                    lineWidth: bw,
+                                    hidden: isNaN(ds.data[i]) || meta.data[i].hidden,
+                                    index: i
+                                };
+                            });
+                        } else {
+                            return [];
+                        }
+                    }
                 }
 
             },
