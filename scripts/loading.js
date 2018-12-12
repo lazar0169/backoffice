8b148320e146131f1622d71f2273e1fa2bbc3cac
Lazar Manasijević <lazar0169@gmail.com>
2018-12-12 15:58:39 +0100

    Loading: added some logs and precentage

--- scripts/loading.js
+++ scripts/loading.js
@@ -1,15 +1,24 @@
 let loading = function () {
     let isLoaded = false;
+    let passed = 0;
     let conditions = {
         'comm/user/role': false,
         'comm/configuration/profile/get': false,
         'comm/dashboard/get': false,
         'comm/currency/get': false
     }
+    let total = Object.keys(conditions).length;
 
     function validate(action) {
         if (conditions[action] !== undefined) {
             conditions[action] = true;
+            passed++
+            $$('#loading-precentage').innerHTML = `LOADING &nbsp;&nbsp; ${Math.ceil((total / passed) * 100)}%`;
+            if (_config.development) {
+                let log = document.createElement('pre');
+                log.innerHTML = `Processing: ${action}`;
+                $$('#loading-log').appendChild(log);
+            }
         }
         for (let condition in conditions) {
             if (!conditions[condition]) {
@@ -22,6 +31,8 @@ let loading = function () {
     function loaded() {
         if (isLoaded) return;
         $$('#loading').classList.add('hidden');
+        $$('#loading-precentage').innerHTML = 'LOADING &nbsp;&nbsp; 0%';
+        $$('#loading-log').innerHTML = '';
         isLoaded = true;
     }
 
