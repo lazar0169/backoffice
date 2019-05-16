16419ad043d20ed34d437d63d4d9cda88a990870
lazar0169 <lazar0169@gmail.com>
2019-05-16 12:20:12 +0200

    Datepicker: reset added and range

--- scripts/components/datepicker.js
+++ scripts/components/datepicker.js
@@ -2,6 +2,14 @@ let datepicker = function () {
     // Initialize all date pickers
     on('load', function () {
         for (let picker of $$('.datepicker')) {
+            let isRange = picker.parentElement.classList.contains('fieldset');
+            let firstPicker;
+            let secondPicker;
+            if (isRange) {
+                firstPicker = picker.parentElement.getElementsByClassName('datepicker')[0];
+                secondPicker = picker.parentElement.getElementsByClassName('datepicker')[1];
+            }
+            picker.isRangeSet = false;
             let dp = new Pikaday({
                 field: picker,
                 firstDay: 1,
@@ -20,9 +28,14 @@ let datepicker = function () {
                 defaultDate: new Date(),
                 onSelect: function (date) {
                     trigger(`date/${picker.id}`, this.toString('YYYY-MM-DDT00:00:00.000Z'));
+                    if (isRange) {
+                        setRange();
+                    }
                     log(this.toString());
                 }
             });
+
+            picker.self = dp;
 
             picker.getDate = () => {
                 return dp.toString('YYYY-MM-DDT00:00:00.000Z');
@@ -33,13 +46,16 @@ let datepicker = function () {
                 dp.gotoDate(new Date(date));
             };
 
-            picker.reset = () => {
-                dp.setMinDate(new Date('2010-01-01T00:00:00Z'));
-            };
-
             picker.setMax = (date) => {
                 dp.setMaxDate(new Date(date));
             };
+
+            picker.setStart = (date) => {
+                dp.setStartRange(new Date(date));
+            }
+            picker.setEnd = (date) => {
+                dp.setEndRange(new Date(date));
+            }
 
             picker.setMin = (date) => {
                 dp.setMinDate(new Date(date));
@@ -48,6 +64,29 @@ let datepicker = function () {
             picker.setToday = () => {
                 dp.setDate(new Date());
             };
+
+            picker.reset = () => {
+                dp.setMinDate(new Date('2010-01-01T00:00:00Z'));
+                picker.isRangeSet = false;
+                picker.setToday();
+                picker.setStart(new Date());
+                picker.setEnd(new Date());
+            };
+
+            function setRange() {
+                let isStart = picker.id === picker.parentElement.getElementsByClassName('datepicker')[0].id;
+                if (isStart) {
+                    firstPicker.setStart(picker.getDate());
+                    secondPicker.setStart(picker.getDate());
+                    if (!secondPicker.isRangeSet) {
+                        secondPicker.setDate(firstPicker.getDate());
+                    }
+                } else {
+                    firstPicker.setEnd(picker.getDate());
+                    secondPicker.setEnd(picker.getDate());
+                    secondPicker.isRangeSet = true;
+                }
+            }
         }
     });
 
