fe9e1d249a1fd24bd3d03531027459e963426bb0
lazar0169 <lazar0169@gmail.com>
2019-06-19 16:05:12 +0200

    Minor typo fixed

--- scripts/loading.js
+++ scripts/loading.js
@@ -15,7 +15,7 @@ let loading = function () {
             conditions[action] = true;
             passed++
             let percentage = Math.ceil(Number(passed / total) * 100);
-            $$('#loading-precentage').innerHTML = `LOADING &nbsp;&nbsp; ${percentage}%`;
+            $$('#loading-percentage').innerHTML = `LOADING &nbsp;&nbsp; ${percentage}%`;
             if (_config.development) {
                 let log = document.createElement('pre');
                 log.innerHTML = `Processing: ${action}`;
@@ -33,7 +33,7 @@ let loading = function () {
     function loaded() {
         if (isLoaded) return;
         $$('#loading').classList.add('hidden');
-        $$('#loading-precentage').innerHTML = 'LOADING &nbsp;&nbsp; 0%';
+        $$('#loading-percentage').innerHTML = 'LOADING &nbsp;&nbsp; 0%';
         $$('#loading-log').innerHTML = '';
 
         let user = $$('#sidebar-user').children[1].innerText.split(' ')[0];
