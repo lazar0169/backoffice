11243c5863407a4504a4ab9f18e3c390bc789bdb
milicke95 <milicevic.v.milan@gmail.com>
2019-07-24 12:18:04 +0200

    Players: player tab dashboard table added, total summary in progress

--- scripts/app/players.js
+++ scripts/app/players.js
@@ -3,9 +3,62 @@ let players = function () {
     let getPlayerButton = $$('#players-get-player');
     let playersSearchWrapper = $$('#players-player-players-search-wrapper');
     let playersListWrapper = $$('#players-player-players-table-wrapper');
+    let playerDataWrapper = $$('#players-player-data-wrapper');
+    let playerDataFlagInteresting = $$('#player-flag-interesting');
+    let playerDataFlagSuspicious = $$('#player-flag-interesting');
+    let playerDataFlagDisable = $$('#player-flag-interesting');
+    let playerDataFlagTest = $$('#player-flag-test');
+    let playerDashboardWrapper = $$('#players-player-data-dashboard-wrapper');
 
-    const showPlayerData = (data) => {
+    const showPlayerData = (data, playerId) => {
+        playerDataWrapper.classList.remove('hidden');
+        showPlayerHeaderData(playerId, data.flags, data.onlineStatus);
+        showPlayerDashboardData(data.dashboard);
+        // showPlayerGroupsData();
+        showPlayerSummaryData(data.info, data.totalStats);
+        // showPlayerPeriodData();
         console.log(data);
+    };
+
+    const showPlayerHeaderData = (id, flags, status) => {
+        $$('#players-player-data-id').innerHTML = id;
+        showPlayerFlagData(flags);
+    };
+
+    const showPlayerFlagData = (flags) => {
+        if (flags.interesting) {
+            playerDataFlagInteresting.checked = true;
+            return;
+        }
+
+        if (flags.suspicious) {
+            playerDataFlagSuspicious.checked = true;
+            return;
+        }
+
+        if (flags.disabled) {
+            playerDataFlagDisable.checked = true;
+            return;
+        }
+
+        if (flags.test) {
+            playerDataFlagTest.checked = true;
+            return;
+        }
+    };
+
+    const showPlayerDashboardData = (data) => {
+        if(playerDashboardWrapper.children.length > 0){
+            playerDashboardWrapper.children[0].remove();
+        }
+        
+        let tableNode = table.generate({
+            data: parsePlayerDashboardData(data),
+            id: 'playerDashboardData',
+            sticky: true,
+            stickyCol: false
+        });
+        playerDashboardWrapper.appendChild(tableNode);
     };
 
     const afterLoad = (tab) => {
@@ -32,6 +85,25 @@ let players = function () {
                 removeLoader($$(`#players-navbar-${tab}`));
             }
         });
+    };
+
+    const showPlayerSummaryData = (info, total) => {
+        clearSummaryData();
+        let infoId = $$('#players-player-data-summary-id');
+        let infoName = $$('#players-player-data-summary-name');
+        let totalBet = $$('#players-player-data-summary-bet');
+        let totalWin = $$('#players-player-data-summary-win');
+        let totalAvgBet = $$('#players-player-data-summary-avg-bet');
+        let totalRounds = $$('#players-player-data-summary-rounds');
+        let totalGgr = $$('#players-player-data-summary-ggr');
+
+        infoId.innerHTML += info.id;
+        infoName.innerHTML += info.name;
+        totalBet.innerHTML += total.bet;
+        totalWin.innerHTML += total.win;
+        totalAvgBet.innerHTML += total.avgBet;
+        totalRounds.innerHTML += total.rounds;
+        totalGgr.innerHTML += total.ggr;
     };
 
     const getOperators = (data, tab) => {
@@ -110,7 +182,7 @@ let players = function () {
             },
             success: function (response) {
                 if (response.responseCode === message.codes.success) {
-                    showPlayerData(response.result);
+                    showPlayerData(response.result, id);
                 } else {
                     trigger('message', response.responseCode);
                 }
@@ -169,7 +241,79 @@ let players = function () {
                 tableRow.style.display = 'none';
             }
         }
+    };
+
+    const parsePlayerDashboardData = (data) => {
+        let firstColKeys = Object.keys(data);
+        let colsKeys = Object.keys(data[firstColKeys[0]]);
+
+        let resultData = [];
+        for (let key of colsKeys) {
+            let row = {
+                ' ': transformCamelToRegular(key)//key.charAt(0).toLocaleUpperCase() + key.slice(1)
+            };
+            for (let firstKey of firstColKeys) {
+                row[firstKey] = data[firstKey][key];
+            }
+            resultData.push(row);
+        }
+        return resultData;
     }
+
+    const clearPlayerFlags = () => {
+        playerDataFlagInteresting.checked = false;
+        playerDataFlagSuspicious.checked = false;
+        playerDataFlagDisable.checked = false;
+        playerDataFlagTest.checked = false;
+    };
+
+    const clearSummaryData = () => {
+        let infoId = $$('#players-player-data-summary-id');
+        let infoName = $$('#players-player-data-summary-name');
+        let totalBet = $$('#players-player-data-summary-bet');
+        let totalWin = $$('#players-player-data-summary-win');
+        let totalAvgBet = $$('#players-player-data-summary-avg-bet');
+        let totalRounds = $$('#players-player-data-summary-rounds');
+        let totalGgr = $$('#players-player-data-summary-ggr');
+
+        infoId.innerHTML = 'Id: ';
+        infoName.innerHTML = 'Name: ';
+        totalBet.innerHTML = 'Bet: ';
+        totalWin.innerHTML = 'Win: ';
+        totalAvgBet.innerHTML = 'Avg Bet: ';
+        totalRounds.innerHTML = 'Rounds: ';
+        totalGgr.innerHTML = 'GGR; ';
+    };
+
+    const playerFlagChanged = () => {
+        if (playerDataFlagInteresting.checked) {
+            playerDataFlagSuspicious.checked = false;
+            playerDataFlagDisable.checked = false;
+            playerDataFlagTest.checked = false;
+            return;
+        }
+
+        if (playerDataFlagSuspicious.checked) {
+            playerDataFlagInteresting.checked = false;
+            playerDataFlagDisable.checked = false;
+            playerDataFlagTest.checked = false;
+            return;
+        }
+
+        if (playerDataFlagDisable.checked) {
+            playerDataFlagSuspicious.checked = false;
+            playerDataFlagInteresting.checked = false;
+            playerDataFlagTest.checked = false;
+            return;
+        }
+
+        if (playerDataFlagTest.checked) {
+            playerDataFlagSuspicious.checked = false;
+            playerDataFlagInteresting.checked = false;
+            playerDataFlagDisable.checked = false;
+            return;
+        }
+    };
 
     on('players/main/loaded', function () {
 
@@ -183,9 +327,16 @@ let players = function () {
         getPlayerButton.classList.add('hidden');
         playersSearchWrapper.classList.add('hidden');
         playersListWrapper.classList.add('hidden');
+        playerDataWrapper.classList.add('hidden');
         clearElement($$(`#players-player-portals-list`));
+        clearPlayerFlags();
         afterLoad(`player`);
     });
 
+    playerDataFlagInteresting.addEventListener('click', playerFlagChanged);
+    playerDataFlagSuspicious.addEventListener('click', playerFlagChanged);
+    playerDataFlagDisable.addEventListener('click', playerFlagChanged);
+    playerDataFlagTest.addEventListener('click', playerFlagChanged);
     getPlayerButton.addEventListener('click', getPlayer);
+    
 }();
