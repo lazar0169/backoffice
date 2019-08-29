81d67688706fd5d64e220dd3d9a75c6f0d75c895
milicke95 <milicevic.v.milan@gmail.com>
2019-08-29 14:01:36 +0200

    History: added files to History folder and change location for transpilation

---
+++ vendor/history/languages/eng.js
@@ -0,0 +1,55 @@
+const ENG = {
+    controls: [
+        ['#find-lang', 'Find'],
+        ['#prev', 'Prev'],
+        ['#next', 'Next'],
+        ['#date-lang', 'DATE'],
+        ['#time-lang', 'TIME'],
+        ['#game-lang', 'GAME'],
+        ['#game-history-lang', 'Game history'],
+        ['#free-spins-lang', 'FREE SPINS'],
+        ['#bonus-symbol-label', 'BONUS SYMBOL'],
+        ['#history-lang', 'HISTORY:'],
+        ['#winning-number-lang', 'WINNING NUMBER: '],
+        ['#message', 'LOADING...'],
+
+        // VIKING BONUS
+        ['#multiplier-title', 'MULTIPLIER'],
+
+        // COM - NECROMANCER BONUS
+        ['#glyph7', 'REPEAT'],
+        ['#glyph8', 'GAME OVER'],
+
+        // TABLE
+        ['#f44', 'EVEN'],
+        ['#f47', 'ODD'],
+
+    ],
+    fragments: {
+        'no-history-message': 'No history found for selected date.',
+        'error-message': 'Something went wrong, history for this date couldn\'t be retrieved.',
+        'winning-numbers': 'WINNING NUMBERS: ',
+        'jackpot-win': 'JACKPOT',
+        'cash': 'CASH',
+        'win': 'WIN',
+        'total-bet': 'TOTAL BET',
+        'lines': 'LINES',
+        'bet': 'BET',
+        'free-spins': 'FREE SPINS',
+        'attempts': 'ATTEMPTS',
+        'gamble-amount': 'GAMBLE AMOUNT',
+        'triple-poker-bet': 'TRIPLE POKER BET',
+        'spin-aborted': 'SPIN ABORTED',
+        'lives': 'LIVES',
+
+        // Datepicker
+        'monthNames': ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        'monthNamesShort': ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        'dayNames': ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+        'dayNamesShort': ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+        'dayNamesMin': ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+        'firstDay': 1,
+        'dateFormat': 'dd M yy'
+    }
+};
+
---
+++ vendor/history/languages/srb.js
@@ -0,0 +1,53 @@
+const SRB = {
+    controls: [
+        ['#find-lang', 'Pronađi'],
+        ['#prev', 'Prethodni'],
+        ['#next', 'Sledeći'],
+        ['#date-lang', 'DATUM'],
+        ['#time-lang', 'VREME'],
+        ['#game-lang', 'IGRA'],
+        ['#game-history-lang', 'Istorija igre'],
+        ['#free-spins-lang', 'BONUS IGRE'],
+        ['#bonus-symbol-label', 'BONUS SIMBOL'],
+        ['#history-lang', 'ISTORIJAT:'],
+        ['#winning-number-lang', 'IZVUČEN BROJ: '],
+        ['#message', 'UČITAVA SE...'],
+
+        // VIKING BONUS
+        ['#multiplier-title', 'MULTIPLIKATOR'],
+
+        // COM - NECROMANCER BONUS
+        ['#glyph7', 'PONOVI'],
+        ['#glyph8', 'KRAJ IGRE'],
+
+        // TABLE
+        ['#f44', 'PAR'],
+        ['#f47', 'NEPAR'],
+    ],
+    fragments: {
+        'no-history-message': 'Istorija za zadati dan nije pronađena.',
+        'error-message': 'Nešto je pošlo po zlu, istorija za ovaj datum nije mogla biti pribavljena.',
+        'winning-numbers': 'DOBITNI BROJEVI: ',
+        'jackpot-win': 'JACKPOT',
+        'cash': 'NOVAC',
+        'win': 'DOBITAK',
+        'total-bet': 'CEO ULOG',
+        'lines': 'LINIJE',
+        'bet': 'ULOG',
+        'free-spins': 'BONUS IGRE',
+        'attempts': 'POKUŠAJI',
+        'gamble-amount': 'ULOG',
+        'triple-poker-bet': 'TRIPLE POKER BONUS',
+        'spin-aborted': 'SPIN OBUSTAVLJEN',
+        'lives': 'ŽIVOT',
+
+        // Datepicker
+        'monthNames': ['Januar', 'Februar', 'Mart', 'April', 'Maj', 'Jun', 'Jul', 'Avgust', 'Septembar', 'Oktobar', 'Novembar', 'Decembar'],
+        'monthNamesShort': ['Jan', 'Feb', 'Mar', 'Apr', 'Maj', 'Jun', 'Jul', 'Avg', 'Sep', 'Okt', 'Nov', 'Dec'],
+        'dayNames': ['Nedelja', 'Ponedeljak', 'Utorak', 'Sreda', 'Četvrtak', 'Petak', 'Subota'],
+        'dayNamesShort': ['Ned', 'Pon', 'Uto', 'Sre', 'Čet', 'Pet', 'Sub'],
+        'dayNamesMin': ['Ne', 'Po', 'Ut', 'Sr', 'Če', 'Pe', 'Su'],
+        'firstDay': 1,
+        'dateFormat': 'dd M yy'
+    }
+};
